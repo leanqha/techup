a30a6840a524e71f2c79e08cc9a9798b6edb34f4
leanq <leanqha@gmail.com>
2025-11-10 12:42:57 +0300

    rewrited some code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -67,7 +67,6 @@ func main() {
 		AllowOrigins: []string{
 			"http://localhost:5173",
 			"https://leanqha.github.io",
-			"*",
 		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
